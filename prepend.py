628edfe1f3b2caa67f7ca201773f0d9f5aca9907
Zach <zdelrosario@outlook.com>
2020-07-08 08:31:42 -0400

    rm pandas dep

--- prepend.py
+++ prepend.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 import os
-from pandas import read_csv
 from shutil import copyfile
 
+## Process the schedule document
+names = []
+days = []
 
-df_schedule = read_csv("./scripts/schedule.csv")
+with open("./scripts/schedule.csv", "r") as file_schedule:
+    ## Dump the header
+    file_schedule.readline()
+    ## Process each line
+    for i, line in enumerate(file_schedule):
+        name, day = line.split(",")
+        names.append(name)
+        days.append(int(day))
+
+## Copy the exercises
 dir_base = os.path.dirname(__file__)
 
-for i in range(df_schedule.shape[0]):
-    source = os.path.join(dir_base, "exercises", df_schedule.name[i] + "-assignment.Rmd")
+for i in range(len(names)):
+    source = os.path.join(dir_base, "exercises", names[i] + "-assignment.Rmd")
     target = os.path.join(
         dir_base,
         "exercises_sequenced",
-        "d{0:02d}-".format(df_schedule.day[i]) + \
-        df_schedule.name[i] + \
+        "d{0:02d}-".format(days[i]) + \
+        names[i] + \
         "-assignment.Rmd"
     )
 
